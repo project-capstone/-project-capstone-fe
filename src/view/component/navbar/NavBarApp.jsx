0c6c211e5cdd906387cefc2e0153059e54fced2e
dickymaulana <dickymaulanaa@email.com>
2021-12-10 22:23:11 +0700

    add navbar-card

---
+++ src/view/component/navbar/NavBarApp.jsx
@@ -0,0 +1,32 @@
+import './NavBarApp.css'
+import { NavDropdown, Navbar, Container, Nav } from "react-bootstrap"
+
+const NavBarApp =() =>{
+    return(
+<Navbar collapseOnSelect expand="lg" variant="dark" className='colorNav'>
+  <Container>
+  <Navbar.Brand href="#home"><h2>Barengin</h2></Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+    </Nav>
+    <Nav>
+      <Nav.Link href="#product" className="mt mx-3" >Product</Nav.Link>
+      <Nav.Link href="#deets" className="mt mx-3">FAQ</Nav.Link>
+      <NavDropdown title={
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" className="" fill="white" class="bi bi-person-circle" viewBox="0 -2 15 24">
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+            </svg>
+          }
+      id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.2">Sign In</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1">Sign Up</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
+export default NavBarApp
