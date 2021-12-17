8880be0d3b81a5ebe8ab056e27867359dbd638a6
Chany Rizqina <40732047+mindty@users.noreply.github.com>
2021-12-17 11:56:09 +0700

    change navigate for admin

--- src/view/component/card/card.jsx
+++ src/view/component/card/card.jsx
@@ -1,73 +1,89 @@
 import { useEffect, useState } from "react";
-import { Card,Button, Row, Col, Spinner } from "react-bootstrap";
+import { Card, Button, Row, Col, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import allStore from "../../../store/actions";
 import "./card.css";
 
-const CardProduct = () =>{
-    // const[loading, setLoading] = useState()
-    const loading = useSelector(({ loading }) => loading);
-    const dispatch = useDispatch();
-    const listProduct = useSelector(({listProduct}) => listProduct)
+const CardProduct = () => {
+  // const[loading, setLoading] = useState()
+  const loading = useSelector(({ loading }) => loading);
+  const dispatch = useDispatch();
+  const listProduct = useSelector(({ listProduct }) => listProduct);
 
-    
-    useEffect(() =>{
-        dispatch(allStore.fetchProduct())
-    },[dispatch])
-    
- 
-    const navigate = useNavigate();
-    const toNavigate = (ID) =>{
-        navigate(`/group/${ID}`)
-        console.log(ID, "id")
+  useEffect(() => {
+    dispatch(allStore.fetchProduct());
+  }, [dispatch]);
+
+  const navigate = useNavigate();
+  const toNavigate = (ID) => {
+    if (localStorage.getItem("role") === "admin") {
+      navigate(`/products/${ID}`);
+    } else {
+      navigate(`/group/${ID}`);
+      console.log(ID, "id");
     }
-    
-    const Rupiah = Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
-    });
-  
-    if(loading){
-        console.log(loading, "loadi")
+  };
 
-        return(
-            <div>
-                <div className="product" id="product"><h3 style={{color:"#0c6632"}}>Product</h3> <br/></div>
-                <div className="containerCard  d-flex flex-wrap justify-content-beetwen">
-                    <div className="loadProduct">
-                        <h4> <Spinner animation="grow"/>Please Wait ...</h4>
-                    </div>
-            </div>
-        </div>
-        )
-    }
-    
-        return (
-            <div>
-                <div className="product" id="product"><h3 style={{color:"#0c6632"}}>Product</h3> <br/></div>
-                <div className="containerCard  d-flex flex-wrap justify-content-beetwen">   
-                   {listProduct.map((el,i) => (
-                            <div className="mx-2 my-3" key={i}>
-                    <Card style={{ width: '16rem' }}>
-                    <Card.Img variant="top" src={el.Url} className="imgProduct"/>
-                    <Card.Body>
-                        <Card.Title>{el.Name_Product}</Card.Title>
-                        <Card.Text>
-                        <Row>
-                            <Col md="5">Price</Col>
-                            <Col>{Rupiah.format(`${el.Price}`)}</Col>
-                        </Row>
-                        </Card.Text>
-                        <Button variant="success" onClick={() =>{ toNavigate(el.ID)}}>More Info</Button>
-                    </Card.Body>
-                    </Card>
-                </div>
-                   ))}
-            </div>
-        </div>
-        )
-    
-}
+  const Rupiah = Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 
-export default CardProduct
+  if (loading) {
+    console.log(loading, "loadi");
+
+    return (
+      <div>
+        <div className="product" id="product">
+          <h3 style={{ color: "#0c6632" }}>Product</h3> <br />
+        </div>
+        <div className="containerCard  d-flex flex-wrap justify-content-beetwen">
+          <div className="loadProduct">
+            <h4>
+              {" "}
+              <Spinner animation="grow" />
+              Please Wait ...
+            </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="product" id="product">
+        <h3 style={{ color: "#0c6632" }}>Product</h3> <br />
+      </div>
+      <div className="containerCard  d-flex flex-wrap justify-content-beetwen">
+        {listProduct.map((el, i) => (
+          <div className="mx-2 my-3" key={i}>
+            <Card style={{ width: "16rem" }}>
+              <Card.Img variant="top" src={el.Url} className="imgProduct" />
+              <Card.Body>
+                <Card.Title>{el.Name_Product}</Card.Title>
+                <Card.Text>
+                  <Row>
+                    <Col md="5">Price</Col>
+                    <Col>{Rupiah.format(`${el.Price}`)}</Col>
+                  </Row>
+                </Card.Text>
+                <Button
+                  variant="success"
+                  onClick={() => {
+                    toNavigate(el.ID);
+                  }}
+                >
+                  More Info
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardProduct;
